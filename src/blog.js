e3bb602e8c322b0ddbde301a752545b12e0d461e
Janne Salokoski <salokoski.janne@gmail.com>
2023-11-15 20:47:40 +0200

    Added dark and light colorschemes

--- src/blog.js
+++ src/blog.js
@@ -1,1 +1,17 @@
-console.log("Hei hei!")
+function get_preferred_colorscheme() {
+  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.getElementById("body").classList.add("dark")
+  }
+}
+
+function toggle_colorscheme() {
+  document.getElementById("body").classList.toggle("dark")
+}
+
+function init() {
+  get_preferred_colorscheme()
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  init()
+})
