f55a8c3bd0f6e6bc2309c8504acfb7480c682552
Janne Salokoski <salokoski.janne@gmail.com>
2023-11-15 18:32:12 +0200

    Add logging

--- src/render-post.py
+++ src/render-post.py
@@ -1,7 +1,30 @@
 #!/bin/env py
 
+import markdown
+import logging
+import os
+import re
+
+logger = logging.getLogger("generate-post")
+logger.setLevel(logging.DEBUG)
+
+stream = logging.StreamHandler()
+stream.setLevel(logging.DEBUG)
+
+stream_formatter = logging.Formatter("%(asctime)s <%(name)s> [%(levelname)s]: %(message)s", datefmt="%H:%M:%S")
+stream.setFormatter(stream_formatter)
+
+logfile = logging.FileHandler("render-post.log", encoding="utf-8")
+logfile.setLevel(logging.DEBUG)
+
+logfile_formatter = logging.Formatter("%(asctime)s <%(name)s> [%(levelname)s]: %(message)s", datefmt="%y-%M-%d %H:%M:%S")
+logfile.setFormatter(logfile_formatter)
+
+logger.addHandler(stream)
+logger.addHandler(logfile)
+
 def main():
-    pass
+    logger.info("Starting 'render-post.py'")
 
 if __name__ == "__main__":
     main()
